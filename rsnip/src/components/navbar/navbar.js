223b0b1bfdfb8d4d392d528bb8d4cf0e8bd8a540
John <jashby@student.neumont.edu>
2022-04-12 18:02:32 -0600

    more nav updates

--- rsnip/src/components/navbar/navbar.js
+++ rsnip/src/components/navbar/navbar.js
@@ -1,22 +1,27 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./navbarStyles.css";
 
 const Navbar = () => {
+  const location = useLocation();
+  const [changeNav, setChangeNav] = useState(false);
+  const changeNavColors = () => {
+    window.scrollY >= 1 ? setChangeNav(true) : setChangeNav(false);
+  };
+  window.addEventListener("scroll", changeNavColors);
   return (
-    <div className=" nav">
-      <div className="content row-space-between">
-        <Link className="nav-title nav-group semi-bold" to="/">
+    <div className={changeNav ? "nav white-bg light-shadow" : "nav white-txt"}>
+      <div className="content row-space-between semi-bold">
+        <Link className="nav-title nav-group" to="/">
           <img class="nav-logo" src="/RSnip_Logo.svg" alt="RSnip Logo" />
           Snip
         </Link>
         <div>
-          <Link className="m-r-1" to="/">
-            Home
-          </Link>
+          {/* <div>{location.pathname}</div> */}
           <Link className="m-r-1" to="/register">
             Sign Up
           </Link>
-          <Link className="btn green-btn m-r-1" to="/login">
+          <Link className="btn green-btn light-shadow m-r-1" to="/login">
             Login
           </Link>
         </div>
--- rsnip/src/pages/home/home.js
+++ rsnip/src/pages/home/home.js
@@ -3,8 +3,14 @@ import BgHeader from "../../assets/images/header-bg.svg";
 const Home = () => {
   return (
     <>
-      <img style={{ width: "100%" }} src={BgHeader} alt="Header Bg" />
-      <div class="content">
+      {/* <img style={{ width: "100%" }} src={BgHeader} alt="Header Bg" /> */}
+      <div
+        style={{ backgroundImage: `url(${BgHeader})`, height: "90vh" }}
+        class="home-header"
+      >
+        <div className="content p-t-5">Test</div>
+      </div>
+      <div className="content">
         <div style={{ height: "100vh" }}>Welcome To The Home Page</div>
         <div className="btn green-btn">TEST BTN</div>
       </div>
