9eb6a9ec8c3ad66fc33fa4c01ccf7061db434f5c
RUnroe <ryanunroe@gmail.com>
2022-05-18 21:32:21 -0600

    Unit test signup page

--- rsnip/src/tests/signup.test.js
+++ rsnip/src/tests/signup.test.js
@@ -4,8 +4,7 @@ import {setupServer} from 'msw/node'
 import {render, fireEvent, waitFor, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Signup from '../pages/signup/signup';
-import Confirmation from '../pages/confirmation/Confirmation';
-import { Router, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const server = setupServer(
   rest.get('/signup', (req, res, ctx) => {
@@ -18,8 +17,8 @@ afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 test('loads and displays signup form', async () => {
-  render(<Router>
- <Signup /></Router>)
+  render(<BrowserRouter>
+ <Signup /></BrowserRouter>)
 
   await waitFor(() => screen.findByTestId('signup-form'))
 
@@ -40,22 +39,21 @@ test('loads and displays signup form', async () => {
 // })
 
 
-// test('displays error message on form submit when form fields are empty', async () => {
-//     render(<Router>      
-//     <Route path="confirmation" element={<Confirmation />}></Route>
-// <Signup /></Router>)
+test('displays error message on form submit when form fields are empty', async () => {
+    render(<BrowserRouter>
+ <Signup /></BrowserRouter>)
 
-//     await waitFor(() => screen.findByTestId('signup-form'))
+    await waitFor(() => screen.findByTestId('signup-form'))
 
-//     fireEvent.click(screen.getByTestId('signup-btn'))
+    fireEvent.click(screen.getByTestId('signup-btn'))
 
-//     await waitFor(() => screen.findByTestId('signup-error-username'))
-//     await waitFor(() => screen.findByTestId('signup-error-email'))
-//     await waitFor(() => screen.findByTestId('signup-error-password'))
-//     await waitFor(() => screen.findByTestId('signup-error-confirm'))
-//     expect(screen.getByTestId('signup-error-username')).not.toBeEmpty();
-//     expect(screen.getByTestId('signup-error-email')).not.toBeEmpty();
-//     expect(screen.getByTestId('signup-error-password')).not.toBeEmpty();
-//     expect(screen.getByTestId('signup-error-confirm')).not.toBeEmpty();
-// });
+    await waitFor(() => screen.findByTestId('signup-error-username'))
+    await waitFor(() => screen.findByTestId('signup-error-email'))
+    await waitFor(() => screen.findByTestId('signup-error-password'))
+    await waitFor(() => screen.findByTestId('signup-error-confirm'))
+    expect(screen.getByTestId('signup-error-username')).not.toBeEmpty();
+    expect(screen.getByTestId('signup-error-email')).not.toBeEmpty();
+    expect(screen.getByTestId('signup-error-password')).not.toBeEmpty();
+    expect(screen.getByTestId('signup-error-confirm')).not.toBeEmpty();
+});
 
