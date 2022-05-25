01c92ed10db54ae5a37c133821d203cb5a955665
RUnroe <ryanunroe@gmail.com>
2022-05-25 08:44:37 -0600

    Prevent logged in users from going to login & signup pages

--- rsnip/src/app.js
+++ rsnip/src/app.js
@@ -12,6 +12,14 @@ import Profile from "./pages/profile/profile";
 import Confirmation from "./pages/confirmation/Confirmation";
 import Search from "./pages/search/search";
 
+
+
+
+const NoAuthRoute = ({user, component}) => {
+  if(user === {} || user === undefined || user === null) return component;
+  return <Navigate to="/" replace />;
+}
+
 const App = () => {
   const [user, setUser] = useState();
   useEffect(() => {
@@ -38,12 +46,12 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<SignUp />} />
+          <Route path="login" element={<NoAuthRoute user={user} component={<Login />}/> } />
+          <Route path="register" element={<NoAuthRoute user={user} component={<SignUp />}/>} />
           <Route path="snippet/edit/:snippetId" element={<SnippetEditor />} />
           <Route path="snippet/:snippetId" element={<ViewSnippet />} />
           <Route path="user/:userId" element={<Profile />} />
-          <Route path="confirmation" element={<Confirmation />}></Route>
+          <Route path="confirmation" element={<Confirmation />} />
           <Route path="search" element={<Search />} />
           <Route
             path="snippet/edit/worker-javascript.js"
